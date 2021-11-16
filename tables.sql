82d5e550134e85ee4ab80e627317fff697701c4b
Harry Bairstow <h.bairstow22@gmail.com>
2021-11-16 16:36:00 +0000

    Update Reports
    * add resolved data
    * add timetsamps

--- tables.sql
+++ tables.sql
@@ -30,19 +30,19 @@ CREATE TABLE IF NOT EXISTS public.users
 
 CREATE TABLE IF NOT EXISTS public.repositories
 (
-    id              uuid primary key not null default uuid_generate_v4(),
-    name            varchar(25)      not null,
-    owner_id        uuid             not null,
+    id            uuid primary key not null default uuid_generate_v4(),
+    name          varchar(25)      not null,
+    owner_id      uuid             not null,
 
     -- Flags
-    flags           bigint           not null default 0, -- All Flags
-    public_flags    bigint           not null default 0, -- Flags Show To Public
-    private_flags   bigint           not null default 0, -- Flags Hidden
+    flags         bigint           not null default 0, -- All Flags
+    public_flags  bigint           not null default 0, -- Flags Show To Public
+    private_flags bigint           not null default 0, -- Flags Hidden
 
     -- Timestamps
-    created_at      timestamptz      not null default now(),
-    edited_at       timestamptz      not null default now(),
-    deleted_at      timestamptz
+    created_at    timestamptz      not null default now(),
+    edited_at     timestamptz      not null default now(),
+    deleted_at    timestamptz
 );
 ALTER TABLE repositories
     ADD CONSTRAINT fk_repository_owner FOREIGN KEY (owner_id) REFERENCES users (id);
@@ -85,13 +85,23 @@ ALTER TABLE member_profile
 
 CREATE TABLE IF NOT EXISTS public.reports
 (
-    id             uuid primary key not null default uuid_generate_v4(),
-    type           int4             not null,
-    reporter_id    uuid             not null,
-    report_content jsonb            not null default '{}'::jsonb
+    id               uuid primary key not null default uuid_generate_v4(),
+    type             int4             not null,
+    reporter_id      uuid             not null,
+    report_content   jsonb            not null default '{}'::jsonb,
+
+    -- Timestamps
+    created_at       timestamptz      not null default now(),
+    
+    -- Resolved Info
+    resolved_at      timestamptz,
+    resolved_message text,
+    resolved_by      uuid
 );
 ALTER TABLE reports
     ADD CONSTRAINT fk_reports_user FOREIGN KEY (reporter_id) REFERENCES users (id);
+ALTER TABLE reports
+    ADD CONSTRAINT fk_reports_resolved_staff FOREIGN KEY (resolved_by) REFERENCES users (id);
 
 CREATE TABLE IF NOT EXISTS public.messages
 (
